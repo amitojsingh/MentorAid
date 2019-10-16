067c6fa3bacfe438740536f9e9f9faca49e7ed7e
amitoj singh <amitojsingh95@gmail.com>
2019-10-15 23:59:48 -0400

    Api route is added

--- APP_API/routes/index.js
+++ APP_API/routes/index.js
@@ -1,12 +1,37 @@
-
 var express = require('express');
 var router = express.Router();
+var LdapAuth = require('ldapauth-fork');
+var basicAuth = require('basic-auth');
+let constants = require('../../CONSTANT');
+let student;
+var ldap = new LdapAuth({
+    url: constants.URL,
+    bindDN: constants.BINDDN,
+    bindCredentials: constants.CREDENTIALS,
+    searchBase: 'ou=student,dc=hazur,dc=org',
+    searchFilter: '(uid={{username}})',
+    reconnect: true
 
-
+});
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
 
+router.post('/submit', (req, res, next) => {
+    console.log(req);
+    ldap.authenticate(req.body.username, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            res.send({ success: false, message: 'authentication failed' });
+        } else if (!user.uid) {
+            console.log("user not found Error");
+            res.send({ success: false, message: 'user not found authentication failed' });
+        } else if (user.uid) {
+            console.log("success : user " + user.uid + " found ");
+            res.send({ success: true, message: 'authentication successfull' });
+        }
+    });
+});
 module.exports = router;
